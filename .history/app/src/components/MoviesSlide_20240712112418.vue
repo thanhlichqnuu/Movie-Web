13dfedc2cfc51e06cda240a0394b254b948ab95d
Dương Thanh Lịch <135508140+thanhlichqnuu@users.noreply.github.com>
2024-07-12 14:55:24 +0700

    modify

---
+++ .history/app/src/components/MoviesSlide_20240712112418.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref, computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useWindowSize } from '@vueuse/core';
+import { useFetchMovies } from '@/composables/useFetchMovies';
+import { useMovieStore } from '@/stores/useMovieStore'; // Optional if you still want to use pinia
+
+const { width: windowWidth } = useWindowSize();
+const isSmallTabletAndMobile = computed(() => windowWidth.value < 768);
+
+const props = defineProps({
+  apiKey: {
+    type: String,
+    required: true,
+  },
+});
+
+const router = useRouter();
+const movieStore = useMovieStore();
+const { movies, isLoading, isError } = useFetchMovies(props.apiKey, movieStore.apiUrls);
+
+const navigateToDetail = (slug) => router.push({ name: 'Detail', params: { slugMovie: slug } });
+</script>
+
+<template>
+  <div v-if="isLoading">Loading...</div>
+  <div v-else-if="isError">Error loading movies!</div>
+  <v-carousel v-else v-if="!isSmallTabletAndMobile" cycle hide-delimiters interval="5000" height="90vh">
+    <v-carousel-item v-for="movie in movies" :key="movie._id">
+      <v-img class="d-flex align-end w-100 h-100" :src="`https://img.ophim.live/uploads/movies/${movie.poster_url}`" loading="lazy" cover alt="poster">
+        <div class="movie-details d-flex align-end ml-3">
+          <v-img :src="`https://img.ophim.live/uploads/movies/${movie.thumb_url}`" loading="lazy" width="150" height="auto" alt="thumbnail" />
+          <div class="w-100 ml-12 mb-8">
+            <div class="movie-title">{{ movie.name }}</div>
+            <div class="movie-origin">{{ movie.origin_name }}</div>
+            <div class="movie-year">{{ movie.year }} | {{ movie.tmdb?.type?.toUpperCase() }}</div>
+            <v-btn color="red-darken-1" height="40" class="mt-3 mb-n1" @click="navigateToDetail(movie.slug)">{{ $t("moreInfo") }}</v-btn>
+          </div>
+        </div>
+      </v-img>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<style scoped>
+.movie-details {
+  background-image: linear-gradient(
+    180deg,
+    transparent 0%,
+    transparent,
+    rgba(0, 0, 0, 0.4) 40%,
+    rgba(0, 0, 0, 0.9) 100%
+  );
+}
+
+.movie-title {
+  font-size: 24px;
+  font-weight: bold;
+  color: white;
+}
+
+.movie-origin {
+  font-size: 20px;
+  color: #d3d3d3;
+}
+
+.movie-year {
+  font-size: 17px;
+  color: white;
+}
+</style>
---
+++ .history/app/src/components/NewlyMovies_20240712115736.vue
@@ -0,0 +1,178 @@
+<script setup>
+import { ref, computed, watch } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+import { useI18n } from "vue-i18n";
+import { useStateThemeStore } from "@/stores/useStateThemeStore";
+import useSWRV, { mutate } from 'swrv';
+
+const { t } = useI18n();
+const movieLabel = computed(() => t("singleMovie"));
+const seriesLabel = computed(() => t("seriesMovie"));
+const themeStore = useStateThemeStore();
+
+const tabs = ref([
+  {
+    name: "Anime",
+    api: "https://ophim1.com/v1/api/danh-sach/hoat-hinh?page=1",
+  },
+  {
+    name: movieLabel,
+    api: "https://ophim1.com/v1/api/danh-sach/phim-le?page=1",
+  },
+  {
+    name: seriesLabel,
+    api: "https://ophim1.com/v1/api/danh-sach/phim-bo?page=1",
+  },
+  {
+    name: "TV Show",
+    api: "https://ophim1.com/v1/api/danh-sach/tv-shows?page=1",
+  },
+]);
+
+const router = useRouter();
+const currentTab = ref(tabs.value[0].name);
+const movies = ref([]);
+
+// SWRV setup
+const fetcher = async (url) => {
+  const { data } = await axios.get(url);
+  return data.items.slice(0, 5);
+};
+
+const { data } = useSWRV(
+  () => tabs.value.find(tab => tab.name === currentTab.value)?.api,
+  fetcher
+);
+
+watch(data, (newMovies) => {
+  if (newMovies) {
+    movies.value = newMovies;
+  }
+  
+});
+
+const changeTab = (tab) => {
+  currentTab.value = tab.name;
+  tabs.value.forEach((t) => (t.isActive = t.name === tab.name));
+  mutate(tab.api);
+};
+
+const navigateToDetail = (slug) => {
+  router.push({ name: "Detail", params: { slugMovie: slug } });
+};
+</script>
+
+<template>
+  <v-main style="margin-top: -6px">
+    <div>
+      <h3 class="d-flex justify-start animate-charcter">
+        {{ $t("lastUpdateLabel") }}
+      </h3>
+      <div class="line" />
+    </div>
+
+    <v-tabs v-model="currentTab">
+      <v-tab
+        v-for="tab in tabs"
+        :key="tab.name"
+        @click="changeTab(tab)"
+        class="tab-active"
+      >
+        {{ tab.name }}
+        <div
+          v-show="tab.isActive"
+          :class="
+            themeStore.isDark ? 'tab-indicator-white' : 'tab-indicator-black'
+          "
+        />
+      </v-tab>
+    </v-tabs>
+
+    <div>
+      <v-card
+        v-for="movie in movies"
+        :key="movie._id"
+        @click="navigateToDetail(movie.slug)"
+        class="mb-3"
+      >
+        <div class="d-flex">
+          <v-img
+            :src="`https://img.ophim.live/uploads/movies/${movie.thumb_url}`"
+            loading="lazy"
+            style="width: 65px; height: auto"
+          ></v-img>
+          <v-list-item class="mt-n5 w-100">
+            <div class="d-flex">
+              <v-card-title class="text-subtitle-2">{{
+                movie.name
+              }}</v-card-title>
+            </div>
+            <v-badge color="red" class="ml-7 mb-2">
+              <template v-slot:badge>
+                <span>{{ movie.year }}</span>
+              </template>
+            </v-badge>
+            <v-badge color="blue" class="ml-12 mb-2">
+              <template v-slot:badge>
+                <span>Season {{ movie.tmdb?.season ?? 1 }}</span>
+              </template>
+            </v-badge>
+            <v-badge color="green" class="mb-2" style="margin-left: 71px">
+              <template v-slot:badge>
+                <span>{{ movie.tmdb.type?.toUpperCase() }}</span>
+              </template>
+            </v-badge>
+          </v-list-item>
+        </div>
+      </v-card>
+    </div>
+  </v-main>
+</template>
+
+<style scoped>
+.tab-indicator-white {
+  height: 3px;
+  background-color: white;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+}
+
+.tab-indicator-black {
+  height: 3px;
+  background-color: black;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+}
+
+.animate-charcter {
+  background-image: linear-gradient(
+    -225deg,
+    #231557 0%,
+    #44107a 29%,
+    #ff1361 67%,
+    #fff800 100%
+  );
+  background-clip: border-box;
+  background-size: 200% auto;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  animation: textclip 2s linear infinite;
+  display: inline-block;
+  font-size: 18px;
+}
+
+@keyframes textclip {
+  to {
+    background-position: 200% center;
+  }
+}
+
+.line {
+  border: 1px solid #b5e745;
+  width: 100%;
+}
+</style>
